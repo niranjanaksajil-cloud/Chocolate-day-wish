af586d5f2a5cbb69d9c6ddb82f36b538dc758244
niranjanaksajil-cloud <niranjanaksajil@gmail.com>
2026-02-08 16:49:37 +0530

    Update and rename README.md to chocolate_day.py

---
+++ chocolate_day.py
@@ -0,0 +1,18 @@
+# Chocolate Day Wish Program 🍫💙
+
+boyfriend = "My Love"
+day = "Chocolate Day"
+
+def chocolate_day_wish(name):
+    print("Happy", day, "🍫")
+    print()
+    print("Dear", Kannetttaa + ",")
+    print("You are sweeter than chocolate and more precious than perfect code.")
+    print("In the program of my life, you are my favorite variable,")
+    print("and my heart runs only for you without any errors.")
+    print()
+    print("Quote:")
+    print('"You are the chocolate that makes my life sweet forever."')
+
+# Run the function
+chocolate_day_wish(Shiyaneee)
